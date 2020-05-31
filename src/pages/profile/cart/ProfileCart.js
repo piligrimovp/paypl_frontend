7bb230310ee9a15b6464f7586370bfff1b30b5fd
p.piligrimov <p.piligrimov@iq-dev.digital>
2020-05-31 12:31:13 +0500

    Cart

--- src/pages/profile/cart/ProfileCart.js
+++ src/pages/profile/cart/ProfileCart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {authFetch} from "../profile/ProfilePage";
-import {Breadcrumb, Button, ButtonToolbar, Card, FormControl, FormGroup, FormLabel} from "react-bootstrap";
+import {Alert, Breadcrumb, Button, ButtonToolbar, Card, FormControl, FormGroup, FormLabel} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
 import ProfileMenu from "../../../components/profileMenu/profileMenu";
 import {Link} from "react-router-dom";
@@ -9,7 +9,11 @@ export default class ProfileCart extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            orders: []
+            orders: [],
+            error: {
+                status: false,
+                message: ''
+            }
         }
     }
 
@@ -24,14 +28,85 @@ export default class ProfileCart extends React.Component {
                 this.setState({
                     orders: data
                 })
-            });
+            }).catch(e => {
+            this.setState({
+                error: {
+                    status: true,
+                    message: 'Ошибка при загрузке корзины'
+                }
+            })
+        });
     }
 
-    onChangeQuantity = ({target: {name, value}}: ChangeEvent<HTMLInputElement>,indexOrder,indexGood) => {
+    onChangeQuantity = ({target: {name, value}}: ChangeEvent<HTMLInputElement>, indexOrder, indexGood) => {
         let orders = this.state.orders;
         orders[indexOrder].goods[indexGood].pivot.quantity = value;
         this.setState({
-            orders:orders
+            orders: orders
+        })
+    }
+
+    BuyOrder = (order,index) => {
+        this.setState({
+            error: {
+                status: false,
+                message: ''
+            }
+        })
+        authFetch(window.HOST + '/orders', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: order.id
+            })
+        }).then(response => {
+            if (response.status === 404) {
+                this.setError('Ошибка при попытке оплаты')
+            } else {
+                let orders = this.state.orders;
+                orders.splice(index, 1);
+                this.setState({
+                    orders: orders
+                });
+            }
+        }).catch(e => {
+            this.setError('Ошибка при попытке оплаты')
+        })
+    }
+
+    setError = (error) => {
+        this.setState({
+            error: {
+                status: true,
+                message: error
+            }
+        })
+    }
+
+
+    DeleteOrder = (order, index) => {
+        this.setState({
+            error: {
+                status: false,
+                message: ''
+            }
+        })
+        authFetch(window.HOST + '/orders/destroy', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: order.id
+            })
+        }).then(response => {
+            if (response.status === 404) {
+                this.setError('Ошибка при удалении')
+            } else {
+                let orders = this.state.orders;
+                orders.splice(index, 1);
+                this.setState({
+                    orders: orders
+                });
+            }
+        }).catch(e => {
+            this.setError('Ошибка при удалении')
         })
     }
 
@@ -49,6 +124,8 @@ export default class ProfileCart extends React.Component {
                 </Breadcrumb>
                 <div className={'row m-0 justify-content-between'}>
                     <div className={'col-10 px-5'}>
+                        {this.state.error.status && <Alert variant={'danger'} className={'m-auto'}>
+                            {this.state.error.message}</Alert>}
                         {this.state.orders.map((item, index) => {
                             return <Card key={index} className={'mt-3'}>
                                 <Card.Body>
@@ -58,7 +135,7 @@ export default class ProfileCart extends React.Component {
                                                 <FormGroup>
                                                     <FormLabel>Товар</FormLabel>
                                                     <Link className={'form-control'}
-                                                            to={`/catalog/${good.category.slug}/${good.slug}`}>{good.name}</Link>
+                                                          to={`/catalog/${good.category.slug}/${good.slug}`}>{good.name}</Link>
                                                 </FormGroup>
                                             </div>
 
@@ -67,14 +144,15 @@ export default class ProfileCart extends React.Component {
                                                 <FormControl type={'range'} name={'quantity'} min={1}
                                                              max={good.is_unlimited ? 50 : good.quantity}
                                                              value={good.pivot.quantity}
-                                                             onChange={e => this.onChangeQuantity(e, index,goodIndex)}/>
+                                                             onChange={e => this.onChangeQuantity(e, index, goodIndex)}/>
                                             </FormGroup>
                                         </div>
                                     })}
                                     <ButtonToolbar className={'d-flex justify-content-end mt-2   '}>
-                                        <Button variant="success">
+                                        <Button variant="success" onClick={() => this.BuyOrder(item, index)}>
                                             Оплатить</Button>
-                                        <Button variant="danger" className={'ml-3'}>
+                                        <Button variant="danger" className={'ml-3'}
+                                                onClick={() => this.DeleteOrder(item, index)}>
                                             Удалить</Button>
                                     </ButtonToolbar>
                                 </Card.Body>
