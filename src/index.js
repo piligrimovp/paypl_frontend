80df493a359fe800df8ea359d9d0682d0491f84a
p.piligrimov <p.piligrimov@iq-dev.digital>
2020-05-31 18:22:55 +0500

    fix routes

--- src/index.js
+++ src/index.js
@@ -10,9 +10,20 @@ import CategoryPage from "./pages/category/CategoryPage";
 import './settings';
 import ProductPage from "./pages/product/ProductPage";
 import AboutPage from "./pages/about/AboutPage";
-import Profile from "./pages/profile/Profile";
-import AdminProfile from "./pages/profile/admin/AdminProfile";
-import Payment from "./pages/profile/payment/Payment";
+import {createAuthProvider} from "./Entity/AuthProvider";
+import AuthPage from "./pages/profile/auth/AuthPage";
+import ProfilePage from "./pages/profile/profile/ProfilePage";
+import ProfileCart from "./pages/profile/cart/ProfileCart";
+import ProfileSellerPage from "./pages/profile/seller/ProfileSellerPage";
+import CreateProduct from "./pages/profile/product/CreateProduct";
+import Sellers from "./pages/profile/admin/sellers/Sellers";
+import PaymentSuccess from "./pages/profile/payment/success/PaymentSuccess";
+import PaymentFail from "./pages/profile/payment/fail/PaymentFail";
+
+export const {getUser} = createAuthProvider();
+let user = !!getUser();
+let seller = getUser() && getUser().seller;
+let admin = getUser() && getUser().is_admin;
 
 ReactDOM.render(
     <>
@@ -21,13 +32,26 @@ ReactDOM.render(
             <main className="pt-5 main mb-5">
                 <Switch>
                     <Route exact path={'/'} component={MainPage}/>
-                    <Route path={'/catalog/:category/:product'} component={() => <ProductPage slug={useParams().product}/>}/>
+                    <Route path={'/catalog/:category/:product'}
+                           component={() => <ProductPage slug={useParams().product}/>}/>
                     <Route path={'/catalog/:category'} component={CategoryPage}/>
                     <Route exact path={'/catalog'} component={CategoryPage}/>
                     <Route exact path={'/about'} component={AboutPage}/>
-                    <Route path={'/profile'} component={Profile} />
-                    <Route path={'/admin'} component={AdminProfile} />
-                    <Route path={'/payment'} component={Payment} />
+
+                    {!user && <Route path={'/profile'} component={AuthPage}/>}
+
+                    {user && <Route exact={true} path={'/profile'} component={ProfilePage}/>}
+                    {user && <Route exact={true} path={'/profile/cart'} component={ProfileCart}/>}
+
+                    {seller && <Route exact={true} path={'/profile/seller'} component={ProfileSellerPage}/>}
+                    {seller && <Route exact={true} path={'/profile/addProduct'} component={CreateProduct}/>}
+                    {seller && <Route exact={true} path={'/profile/products'} component={ProfilePage}/>}
+
+                    {admin && <Route exact={true} path={'/admin/sellers'} component={Sellers}/>}
+
+                    <Route path={'/payment/success'} component={PaymentSuccess}/>
+                    <Route path={'/payment/fail'} component={PaymentFail}/>
+
                     <Route path={'/'} component={ErrorPath}/>
                 </Switch>
             </main>
